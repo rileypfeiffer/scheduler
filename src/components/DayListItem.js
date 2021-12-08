2babed1544496bd0557b2dabae8d6772dbad4c90
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-12-08 09:58:55 -0500

    first commit including everything so far

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+const formatSpots = (spots) => {
+  if (!spots) {
+    return "no spots remaining";
+  }
+  if (spots === 1) {
+    return `${spots} spot remaining`
+  }
+  return `${spots} spots remaining`;
+};
+
+export default function DayListItem(props) {
+  const remainingSpots = formatSpots(props.spots);
+  const dayListClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  })
+
+  return (
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{remainingSpots}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewerListClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+  return (
+    <li className={interviewerListClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
