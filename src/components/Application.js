fcdcd8b095b6eaa861256d689dfa57f530e8a2c8
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-12-08 20:35:32 -0500

    import schedule to Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,47 @@
 import React, { useState } from "react";
 import "components/Application.scss";
-import DayList from "./DayList";
+import DayList from "components/DayList";
+import "components/Appointment";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -22,6 +63,9 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const appointmentItems = appointments.map((appointment) => 
+  <Appointment key={appointment.id} {...appointment} />
+);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -41,7 +85,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentItems}
       </section>
     </main>
     
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,7 +7,7 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": props.selected
   })
   return (
-    <li className={interviewerListClass} onClick={() => props.setInterviewer}>
+    <li className={interviewerListClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
