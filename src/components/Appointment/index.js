51b7c0377dedb628f5701279c23fce86f7de4821
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-12-15 23:08:26 -0500

    More debugging to fix saving interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "components/Appointment/styles.scss"
+import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
@@ -25,9 +26,11 @@ export default function Appointment(props) {
   );
 
   function save(name, interviewer) {
+    if (!name || !interviewer) {
+      return;
+    }
 
-    if (name && interviewer) {
-      transition(SAVING);
+    transition(SAVING);
 
       const interview = {
         student: name,
@@ -38,7 +41,6 @@ export default function Appointment(props) {
       .then(() => transition(SHOW))
       .catch(() => transition(ERROR_SAVE, true))
     }
-  }
 
   function deleteInterview(student, interviewer) {
       transition(DELETING, true);
@@ -53,37 +55,32 @@ export default function Appointment(props) {
       .catch(error => transition(ERROR_DELETE, true))
     }
 
-  function edit() {
-    transition(EDIT);
-  }
-
   return (
     <article className="appointment">
+      <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={deleteInterview}
-          onEdit={edit}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === EDIT && (
         <Form
-        name={props.name ? props.name : props.interview.student}
-        value={props.value ? props.value : props.interview.interviewer.id}
+        student={props.interview.student}
+        interviewer={props.interview.interviewer.id}
         interviewers={props.interviewers}
         onSave={save}
-        onCancel={back}
+        onCancel={() => back(SHOW)}
         />
       )}
       {mode === CREATE && (
         <Form
-          name={props.name}
-          value={props.value}
           interviewers={props.interviewers}
           onSave={save}
-          onCancel={back}
+          onCancel={() => back(EMPTY)}
         />
       )}
       {mode === SAVING && <Status message={"Saving"} />}
