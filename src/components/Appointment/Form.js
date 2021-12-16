54465f930b40374c6900de9c2d4c8c541c726174
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-12-15 19:45:28 -0500

    Add more tests for form validation, add JSX for error message to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -41,6 +41,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           value={interviewer}
