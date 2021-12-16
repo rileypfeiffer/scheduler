390c6c9dd47c709e224546595d73be6a4643a4b8
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-12-16 15:02:14 -0500

    Fix error where 4pm-5pm timeslot was not available

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,6 +54,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
